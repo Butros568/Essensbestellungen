a12076662eb98fc4a83f182c8cc1a7d38c9acf42
Geret Wessling <g.wessling@leannova.de>
2024-07-11 12:58:10 +0200

    ..

--- App/models.py
+++ App/models.py
@@ -6,3 +6,23 @@ class Resturant(models.Model):
     name = models.CharField(max_length=30)
     ort = models.CharField(max_length=30)
     url = models.CharField(max_length=30)
+    
+
+class Benutzer(models.Model):
+    mail = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+
+class Items(models.Model):
+    id = models.ImageField()
+    preis = models.IntegerField()
+    bezahlt = models.BooleanField()
+    benutzer = models.ForeignKey(Benutzer)
+    gericht = models.CharField(max_length=30)
+
+
+class Bestellungen(models.Model):
+    id = models.IntegerField()
+    benutzer = models.ForeignKey(Benutzer)
+    items = models.ForeignKey(Benutzer)
