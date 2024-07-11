a756cd3a8906692f608d68bed903fac919133774
Geret Wessling <g.wessling@leannova.de>
2024-07-11 12:07:07 +0200

    ..

--- App/models.py
+++ App/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Resturant(models.Model):
+    id = models.IntegerField()
+    name = models.CharField(max_length=30)
+    ort = models.CharField(max_length=30)
+    url = models.CharField(max_length=30)
